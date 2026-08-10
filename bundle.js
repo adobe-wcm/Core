037060fd7af9f6c63716d5a3878e3cffcda617df
adobe-wcm <mail2nickheart@gmail.com>
2026-08-10 13:44:39 +0530

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,15 +1,5 @@
-const $el = jQuery('#utility_links_sites_apps');
-$el.tooltip('show');
-
-setTimeout(() => {
-  const tip = document.querySelector('.cat-tooltip');
-  const arrow = tip.querySelector('.arrow');
-  const inner = tip.querySelector('.tooltip-inner');
-  console.log({
-    placement: tip.getAttribute('x-placement'),
-    arrowStyle: arrow.getAttribute('style'),
-    arrowRect: arrow.getBoundingClientRect(),
-    innerRect: inner.getBoundingClientRect(),
-    tipRect: tip.getBoundingClientRect()
-  });
-}, 300);
+function initTooltips(context) {
+  if (!$.fn.tooltip || !$.fn.tooltip.Constructor || !$.fn.tooltip.Constructor.VERSION) {
+    return;
+  }
+  ...
