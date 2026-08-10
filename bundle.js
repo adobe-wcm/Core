eed4ddbe4923cd841ae319b40c77db9c5dd7238f
adobe-wcm <mail2nickheart@gmail.com>
2026-08-10 13:00:41 +0530

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,10 +1,15 @@
-const tip = document.querySelector('.cat-tooltip');
-const arrow = tip.querySelector('.arrow');
-const inner = tip.querySelector('.tooltip-inner');
-console.log({
-  placement: tip.getAttribute('x-placement'),
-  arrowStyle: arrow.getAttribute('style'),
-  arrowRect: arrow.getBoundingClientRect(),
-  innerRect: inner.getBoundingClientRect(),
-  tipRect: tip.getBoundingClientRect()
-});
+const $el = jQuery('#utility_links_sites_apps');
+$el.tooltip('show');
+
+setTimeout(() => {
+  const tip = document.querySelector('.cat-tooltip');
+  const arrow = tip.querySelector('.arrow');
+  const inner = tip.querySelector('.tooltip-inner');
+  console.log({
+    placement: tip.getAttribute('x-placement'),
+    arrowStyle: arrow.getAttribute('style'),
+    arrowRect: arrow.getBoundingClientRect(),
+    innerRect: inner.getBoundingClientRect(),
+    tipRect: tip.getBoundingClientRect()
+  });
+}, 300);
