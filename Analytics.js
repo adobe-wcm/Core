6234ca9cdb2c7b056732fb2992ee279abd7b07b5
adobe-wcm <mail2nickheart@gmail.com>
2026-08-11 18:28:45 +0530

    Create Analytics.js

---
+++ Analytics.js
@@ -0,0 +1,11 @@
+<script>
+window.addEventListener('load', function() {
+  requestIdleCallback(function() {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s);j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-XXXXXXX');
+  });
+});
+</script>
