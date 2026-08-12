562f21387851a5da57e6885f52d0aab307840825
adobe-wcm <mail2nickheart@gmail.com>
2026-08-12 17:45:03 +0530

    Update query.js

--- query.js
+++ query.js
@@ -1,7 +1,7 @@
-path=/content/catdotcom
-property=sling:resourceType
-property.value=pqr/components/content/tooltip
-p.limit=-1
+javascript:(function(){var u=new URL(location.href);u.searchParams.set('wcmmode','disabled');location.href=u.href;})();
 
+javascript:(function(){var u=new URL(location.href);u.searchParams.delete('wcmmode');location.href=u.href;})();
 
-/jcr:root/content/catdotcom//element(*)[@sling:resourceType='deg/components/content/product/editable/common/pim/productCards/v9/productCards']
+javascript:(function(){var u=new URL(location.href);if(u.pathname.indexOf('/content')===0){u.pathname='/editor.html'+u.pathname;location.href=u.href;}})();
+
+javascript:(function(){var u=new URL(location.href);if(u.pathname.indexOf('/editor.html/')===0){u.pathname=u.pathname.replace('/editor.html','');location.href=u.href;}})();
