271e1a4a8228577e0e422c505384b4bd02e1d600
adobe-wcm <mail2nickheart@gmail.com>
2026-08-12 17:28:47 +0530

    Update query.js

--- query.js
+++ query.js
@@ -2,3 +2,6 @@ path=/content/catdotcom
 property=sling:resourceType
 property.value=pqr/components/content/tooltip
 p.limit=-1
+
+
+/jcr:root/content/catdotcom//element(*)[@sling:resourceType='deg/components/content/product/editable/common/pim/productCards/v9/productCards']
