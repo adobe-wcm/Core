c2fb1e5d4055a280ecca495622b26c39f96b4476
adobe-wcm <mail2nickheart@gmail.com>
2026-08-13 19:29:16 +0530

    Update query.js

--- query.js
+++ query.js
@@ -1,7 +1,59 @@
-javascript:(function(){var u=new URL(location.href);u.searchParams.set('wcmmode','disabled');location.href=u.href;})();
+window.onYouTubeIframeAPIReady_cookiePolicy = function() {
+    if (typeof OnetrustActiveGroups === 'undefined' ||
+        OnetrustActiveGroups == null ||
+        !OnetrustActiveGroups.includes('C0004')) {
+        return;
+    }
 
-javascript:(function(){var u=new URL(location.href);u.searchParams.delete('wcmmode');location.href=u.href;})();
+    if (window.__degYtPlayersInitialized) {
+        return;
+    }
 
-javascript:(function(){var u=new URL(location.href);if(u.pathname.indexOf('/content')===0){u.pathname='/editor.html'+u.pathname;location.href=u.href;}})();
+    var createPlayers = function() {
+        if (window.__degYtPlayersInitialized) {
+            return;
+        }
 
-javascript:(function(){var u=new URL(location.href);if(u.pathname.indexOf('/editor.html/')===0){u.pathname=u.pathname.replace('/editor.html','');location.href=u.href;}})();
+        var $gallery = $('.pdp-gallery iframe');
+        var $technology = $('.technology iframe');
+
+        // Don't latch the flag on pages with no videos (scenario M)
+        if (!$gallery.length && !$technology.length) {
+            return;
+        }
+
+        window.__degYtPlayersInitialized = true;
+
+        /* ytplayer for pdpGallery */
+        $gallery.each(function (index, value) {
+            /* >>> PASTE EXISTING BODY FROM LINES 470-489, UNCHANGED <<< */
+        });
+
+        /* ytplayer for pdpTechnology */
+        $technology.each(function (index, value) {
+            /* >>> PASTE EXISTING BODY FROM LINES 492-507, UNCHANGED <<< */
+        });
+    };
+
+    if (window.YT && window.YT.Player) {
+        createPlayers();
+        return;
+    }
+
+    var previousReady = window.onYouTubeIframeAPIReady;
+    window.onYouTubeIframeAPIReady = function() {
+        if (typeof previousReady === 'function') {
+            previousReady();
+        }
+        createPlayers();
+    };
+
+    // This code loads the IFrame Player API code asynchronously.
+    // validation to prevent load the script multiple times
+    if (!$('script[src="https://www.youtube.com/iframe_api"]').length) {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+};
