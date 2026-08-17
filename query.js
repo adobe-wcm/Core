b336d6961132127b02c23576e12fe375ff3b21ec
adobe-wcm <mail2nickheart@gmail.com>
2026-08-17 13:34:07 +0530

    Update query.js

--- query.js
+++ query.js
@@ -1,34 +1,14 @@
-// Build the image+button overlay with DOM APIs (no innerHTML).
-var holder = document.createElement('div');
-holder.className = 'holder-yt';
+// Allow-list: a YouTube video id is exactly 11 chars of [A-Za-z0-9_-].
+if (!/^[A-Za-z0-9_-]{11}$/.test(id)) { continue; }
 
-var titleDiv = document.createElement('div');
-titleDiv.id = id + '_title';
-holder.appendChild(titleDiv);
+iframe = document.createElement('iframe');
+    iframe.src = 'https://' + youtubePlayer + '/embed/' + id +
+                 '/?autohide=1&border=0&wmode=opaque&enablejsapi=1';
+    iframe.width = w;
+    iframe.height = h;
+    iframe.frameBorder = 0;
+    iframe.allow = 'fullscreen';
+    iframe.allowFullscreen = true;
 
-var link = document.createElement('a');
-link.href = '#';
-link.id = 'cat-youtubevid-' + id;
-(function (vidId, vw, vh) {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        LoadYoutubeVidOnPreviewClick(vidId, vw, vh);
-    });
-})(id, w, h);
-
-var img = document.createElement('img');
-img.src = 'https://i.ytimg.com/vi/' + id + '/hqdefault.jpg';
-img.alt = '';
-img.style.width = w + 'px';
-img.style.height = h + 'px';
-link.appendChild(img);
-
-var playBtn = document.createElement('div');
-playBtn.setAttribute('style', YT_PLAY_BTN_STYLE);
-link.appendChild(playBtn);
-
-holder.appendChild(link);
-
-// Replace the iframe with the image+button node.
-frame.parentNode.replaceChild(holder, frame);
-i--;
+    div = document.getElementById("cat-youtubevid-" + id);
+    div.parentNode.replaceChild(iframe, div);
