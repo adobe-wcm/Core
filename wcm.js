6c010cc58ce6f8747bbc10a9a08134e5b0d5a462
adobe-wcm <mail2nickheart@gmail.com>
2026-08-21 20:05:15 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,15 +1,5 @@
-const $s = $('#tabs-100a56e058-item-f408fb6881-tabpanel').closest('.slick-slider');
-console.table($s.find('.slick-slide').map(function(i, el) {
-  const cs = getComputedStyle(el);
-  return {
-    idx: el.getAttribute('data-slick-index'),
-    active: el.classList.contains('slick-active'),
-    ariaHidden: el.getAttribute('aria-hidden'),
-    opacity: cs.opacity,
-    z: cs.zIndex,
-    left: cs.left,
-    h: Math.round(el.getBoundingClientRect().height),
-    top: Math.round(el.getBoundingClientRect().top)
-  };
-}).get());
-console.log('track h:', $s.find('.slick-track').height(), 'list h:', $s.find('.slick-list').height());
+$('#tabs-100a56e058-item-f408fb6881-tabpanel').closest('.slick-slider').find('.slick-list')[0].style.height
+
+requestAnimationFrame(function () {
+  $(panel).closest('.slick-initialized').slick('setPosition');
+});
