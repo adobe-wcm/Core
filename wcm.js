f559fda1191605d73d8a427ce6df2c4639046804
adobe-wcm <mail2nickheart@gmail.com>
2026-08-21 19:59:06 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,2 +1,11 @@
-const $s = $('#tabs-100a56e058-item-f408fb6881-tabpanel .slick-slider');
-console.log($s.width(), $s.find('.slick-slide').first().width());
+const $p = $('#tabs-100a56e058-item-f408fb6881-tabpanel');
+const $s = $p.closest('.slick-slider');
+const $sl = $s.find('.slick-slide').first();
+console.log({
+  panel: $p.length,
+  slider: $s.length,
+  sliderW: $s.width(),
+  listW: $s.find('.slick-list').width(),
+  slideInline: $sl[0].style.width,
+  trackLeft: $s.find('.slick-track')[0].style.left
+});
