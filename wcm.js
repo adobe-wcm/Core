763c6883e301fe176aac76558105902943cf5864
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 00:58:50 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,3 +1,11 @@
+$('.carousel--hero .slick-slide').map((i,s)=>{
+  const im = $(s).find('.teaser__img-wrap img')[0];
+  return $(s).attr('data-slick-index') + ' | ' +
+    ($(s).hasClass('slick-cloned')?'clone':'real') + ' | src:' +
+    (im && im.getAttribute('src') ? 'yes':'NO') + ' | ' + (im ? im.className : '-');
+}).get()
+
+
 that.on("init setPosition breakpoint afterChange", function () {
     that.find('.slick-cloned img[data-src]:not([src])').each(function () {
         if (window.lazySizes) {
