333e5d695e1b476af202e35c8f3f513f0cdea158
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 00:38:24 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,12 +1,19 @@
 $('.carousel--hero .slick-slide').map((i,s)=>$(s).find('.teaser').attr('style')).get()
 
-const $w = $('.carousel--hero .carousel__wrapper');
-$('.carousel--hero .slick-track').css('width');           // 4840 or 2200?
-$w.slick('getSlick').options.infinite;                    // still true?
-$w.slick('getSlick').slideCount;
-$('.carousel--hero .carousel__pagination__single').length; // 5 or 10?
-Object.keys($._data($w[0],'events') || {});                // duplicate afterChange?
-$('[id="showhide"]').length;                               // duplicate IDs
+(() => {
+  const $w = $('.carousel--hero .carousel__wrapper');
+  const s = $w.slick('getSlick');
+  return {
+    trackWidth: $('.carousel--hero .slick-track').css('width'),
+    infinite: s.options.infinite,
+    slideCount: s.slideCount,
+    domSlides: $('.carousel--hero .slick-slide').length,
+    cloned: $('.carousel--hero .slick-cloned').length,
+    dots: $('.carousel--hero .carousel__pagination__single').length,
+    events: Object.keys($._data($w[0], 'events') || {}),
+    wrappers: $('.carousel--hero .carousel__wrapper').length
+  };
+})()                               // duplicate IDs
 
 
 that.on('init setPosition breakpoint', function () {
