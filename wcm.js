572666adf5c07941b3c40fb84faf8027c03d7351
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 01:09:30 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,24 +1,5 @@
-// Unveil lazysizes images inside a slick carousel.
-// Slick moves slides via transform, which fires no scroll/resize,
-// so lazysizes never re-evaluates offscreen slides on slide change.
-function unveilSlides($ctx) {
-    if (!window.lazySizes || !$ctx || !$ctx.length) {
-        return;
-    }
-    $ctx.find('.lazyload').each(function () {
-        lazySizes.loader.unveil(this);
-    });
-}
-
-
-that.on("init setPosition breakpoint", function () {
-        that.find(".slick-slide .teaser").matchHeight({ byRow: false });
-
-
-
-  
-
-        $('.carousel--hero .slick-slide').map((i,s)=>{
-  const im=$(s).find('.teaser__img-wrap img')[0];
-  return $(s).attr('data-slick-index')+' | '+(im&&im.getAttribute('src')?'yes':'NO');
-}).get()
+const t = $('.carousel--hero .slick-slide[data-slick-index="4"] .teaser')[0];
+getComputedStyle(t).height;
+$(t).css('display');
+getComputedStyle(t.querySelector('.row')).height;
+getComputedStyle(t.querySelector('.teaser__text-wrap')).position;
