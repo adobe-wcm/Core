de69e27804a1971096d164f3b3ef725dbbd5ebdb
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 00:50:49 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,5 +1,6 @@
-that.on("init setPosition breakpoint", function () {
-    that.find(".slick-slide .teaser").matchHeight({ byRow: false });
-    fixCarouselSlideAccessibility(that);
-    fixCarouselDotsAccessibility(that);
-});
+// 1. Did the fix land?
+$('.carousel--hero .slick-slide').map((i,s)=>
+  String($(s).find('.teaser').attr('style'))).get()
+
+// 2. Is the handler bound?
+$._data($('.carousel--hero .carousel__wrapper')[0],'events').setPosition
