29f962b39475ea8f39b5f79b519d4e8a9a174022
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 00:53:41 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,6 +1,9 @@
-// 1. Did the fix land?
 $('.carousel--hero .slick-slide').map((i,s)=>
-  String($(s).find('.teaser').attr('style'))).get()
+  $(s).attr('data-slick-index')+' | slide '+$(s).height()+
+  ' | teaser '+$(s).find('.teaser').height()+
+  ' | img '+$(s).find('.teaser__img-wrap img').height()+
+  ' | text '+$(s).find('.teaser__text-wrap').height()).get()
 
-// 2. Is the handler bound?
-$._data($('.carousel--hero .carousel__wrapper')[0],'events').setPosition
+
+$('.carousel--hero .slick-track').height()
+$('.carousel--hero .slick-list').height()
