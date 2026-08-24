c5bfa9e245f580728f238a6165c91ddbd1609c3e
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 01:02:15 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,18 +1,24 @@
-$('.carousel--hero .slick-slide').map((i,s)=>{
-  const im = $(s).find('.teaser__img-wrap img')[0];
-  return $(s).attr('data-slick-index') + ' | ' +
-    ($(s).hasClass('slick-cloned')?'clone':'real') + ' | src:' +
-    (im && im.getAttribute('src') ? 'yes':'NO') + ' | ' + (im ? im.className : '-');
-}).get()
-
-
-that.on("init setPosition breakpoint afterChange", function () {
-    that.find('.slick-cloned img[data-src]:not([src])').each(function () {
-        if (window.lazySizes) {
-            lazySizes.loader.unveil(this);
-        } else {
-            this.src = this.getAttribute('data-src');
-            $(this).removeClass('lazyload').addClass('lazyloaded');
-        }
+// Unveil lazysizes images inside a slick carousel.
+// Slick moves slides via transform, which fires no scroll/resize,
+// so lazysizes never re-evaluates offscreen slides on slide change.
+function unveilSlides($ctx) {
+    if (!window.lazySizes || !$ctx || !$ctx.length) {
+        return;
+    }
+    $ctx.find('.lazyload').each(function () {
+        lazySizes.loader.unveil(this);
     });
-});
+}
+
+
+that.on("init setPosition breakpoint", function () {
+        that.find(".slick-slide .teaser").matchHeight({ byRow: false });
+
+
+
+  
+
+        $('.carousel--hero .slick-slide').map((i,s)=>{
+  const im=$(s).find('.teaser__img-wrap img')[0];
+  return $(s).attr('data-slick-index')+' | '+(im&&im.getAttribute('src')?'yes':'NO');
+}).get()
