e88adf8c4c42db46c8df7037bd22d22ab16761d6
adobe-wcm <mail2nickheart@gmail.com>
2026-08-25 00:36:25 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,5 +1,14 @@
 $('.carousel--hero .slick-slide').map((i,s)=>$(s).find('.teaser').attr('style')).get()
 
+const $w = $('.carousel--hero .carousel__wrapper');
+$('.carousel--hero .slick-track').css('width');           // 4840 or 2200?
+$w.slick('getSlick').options.infinite;                    // still true?
+$w.slick('getSlick').slideCount;
+$('.carousel--hero .carousel__pagination__single').length; // 5 or 10?
+Object.keys($._data($w[0],'events') || {});                // duplicate afterChange?
+$('[id="showhide"]').length;                               // duplicate IDs
+
+
 that.on('init setPosition breakpoint', function () {
     that.find('.slick-slide .teaser').matchHeight({ byRow: false });
     fixCarouselSlideAccessibility(that);
