3500c852eb5ba6fd7a5ed32fe728d09c23f7da5b
adobe-wcm <mail2nickheart@gmail.com>
2026-09-02 23:44:27 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,27 +1,29 @@
-function checkAllExpandedButton() {
+$compareHeader.on('click', function () {
   if (checkAllExpandedButton.animating) { return; }
   checkAllExpandedButton.animating = true;
 
-  const $btn = $(this);
-  const collapse = isAllExpandedButton();
+  const $header = $(this);
 
-  if (collapse) {
-    // collapse all and change button text
-    const $inner = $compareHeader.addClass('closed').nextUntil($compareHeader).find('.cell--inner');
+  if ($header.hasClass('closed')) {
+    // show things
+    const $inner = $header.removeClass('closed').nextUntil($compareHeader).show().find('.cell--inner');
+    $inner.slideDown();
+    $inner.promise().done(() => {
+      checkAllExpandedButton.animating = false;
+    });
+  } else {
+    // hide things
+    const $inner = $header.addClass('closed').nextUntil($compareHeader).find('.cell--inner');
     $inner.slideUp();
-    $btn.text($btn.data('open'));
     $inner.promise().done(() => {
       $inner.closest('tr').hide();
       checkAllExpandedButton.animating = false;
     });
-
-  } else {
-    // expand all and change button text
-    const $inner = $compareHeader.removeClass('closed').nextUntil($compareHeader).show().find('.cell--inner');
-    $inner.slideDown();
-    $btn.text($btn.data('close'));
-    $inner.promise().done(() => {
-      checkAllExpandedButton.animating = false;
-    });
   }
-}
+
+  if (isAllExpandedButton()) {
+    $compareExpandCollapseBtn.text($compareExpandCollapseBtn.data('close'));
+  } else {
+    $compareExpandCollapseBtn.text($compareExpandCollapseBtn.data('open'));
+  }
+});
