322e541ba371445096b75b74b0a5b090adeb02ca
adobe-wcm <mail2nickheart@gmail.com>
2026-09-02 23:41:38 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,3 +1,27 @@
-dataLayer.length          // note it
-window.dlWatch = [];
-dataLayer.push = function(o){ dlWatch.push(o); Array.prototype.push.call(dataLayer,o); return dataLayer.length; }
+function checkAllExpandedButton() {
+  if (checkAllExpandedButton.animating) { return; }
+  checkAllExpandedButton.animating = true;
+
+  const $btn = $(this);
+  const collapse = isAllExpandedButton();
+
+  if (collapse) {
+    // collapse all and change button text
+    const $inner = $compareHeader.addClass('closed').nextUntil($compareHeader).find('.cell--inner');
+    $inner.slideUp();
+    $btn.text($btn.data('open'));
+    $inner.promise().done(() => {
+      $inner.closest('tr').hide();
+      checkAllExpandedButton.animating = false;
+    });
+
+  } else {
+    // expand all and change button text
+    const $inner = $compareHeader.removeClass('closed').nextUntil($compareHeader).show().find('.cell--inner');
+    $inner.slideDown();
+    $btn.text($btn.data('close'));
+    $inner.promise().done(() => {
+      checkAllExpandedButton.animating = false;
+    });
+  }
+}
