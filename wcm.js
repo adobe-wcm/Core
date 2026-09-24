505027dff8968de666106a875e5c192c39fa0a94
adobe-wcm <mail2nickheart@gmail.com>
2026-09-25 01:08:41 +0530

    Update wcm.js

--- wcm.js
+++ wcm.js
@@ -1,1 +1,7 @@
-jsProcessor="[default:none,min:gcc;compilationLevel=whitespace;languageIn=ECMASCRIPT_2020;languageOut=ECMASCRIPT_2020]"
+(async () => {
+  const html = await (await fetch(location.href, { cache: 'no-store' })).text();
+  const min = html.replace(/<!--(?!\[if)[\s\S]*?-->/g, '').replace(/>\s+</g, '><').replace(/\s{2,}/g, ' ');
+  const gz = async s => (await new Response(new Blob([s]).stream().pipeThrough(new CompressionStream('gzip'))).arrayBuffer()).byteLength;
+  const [a, b] = [await gz(html), await gz(min)];
+  console.log(`Raw ${(html.length/1024).toFixed(1)} KB → ${(min.length/1024).toFixed(1)} KB | Gzipped ${(a/1024).toFixed(1)} KB → ${(b/1024).toFixed(1)} KB | Real saving: ${((1-b/a)*100).toFixed(1)}%`);
+})();
